82dc54d81a67815cc41f2d7861931ad768de4e77
BigMandu <2only1@naver.com>
2021-04-02 19:18:56 +0900

    Create project
    프로젝트 생성

---
+++ Source/OpenWorldRPG/OpenWorldRPGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OpenWorldRPGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENWORLDRPG_API AOpenWorldRPGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
