74005928c5a100360c30f56141f79913bfa20e9f
BigMandu <2only1@naver.com>
2023-04-09 18:00:01 +0900

    adf
    saas

--- Source/OpenWorldRPG/AI/BT/BTTask_GetChaseLocation.cpp
+++ Source/OpenWorldRPG/AI/BT/BTTask_GetChaseLocation.cpp
@@ -5,6 +5,9 @@
 #include "BehaviorTree/Blackboard/BlackboardKeyType.h"
 #include "BehaviorTree/Blackboard/BlackboardKeyType_Vector.h"
 #include "BehaviorTree/Blackboard/BlackboardKeyType_Rotator.h"
+#include <DrawDebugHelpers.h>
+#include <OpenWorldRPG/BaseCharacter.h>
+#include <OpenWorldRPG/AI/EnemyAIController.h>
 //#include "OpenWorldRPG/AI/EnemyAIController.h"
 
 UBTTask_GetChaseLocation::UBTTask_GetChaseLocation()
@@ -36,7 +39,17 @@ EBTNodeResult::Type UBTTask_GetChaseLocation::ExecuteTask(UBehaviorTreeComponent
 	
 	if(AICon && BBComp)
 	{
-		FVector CurrentTargetLocation = Cast<AActor>(BBComp->GetValueAsObject(AICon->EnemyKey))->GetActorLocation();
+		/*Character의 Canbeseenfrom은 Head Socket을 가지고 인식하기 때문에
+		* 여기서 Target값과 아래 인식한 Location의 Zvalue가 서로 달라 예외처리로 빠져 문제가 발생하여
+		* CurrentTargetLoc값을 인식값과 같도록 HeadSocket의 위치로 함.
+		*/
+		/*FVector CurrentTargetLocation = Cast<AActor>(BBComp->GetValueAsObject(AICon->EnemyKey))->GetActorLocation();*/
+
+		ABaseCharacter* Target = Cast<ABaseCharacter>(BBComp->GetValueAsObject(AICon->EnemyKey));
+		if(Target == nullptr ) return EBTNodeResult::Failed;
+		FVector CurrentTargetLocation = Target->GetMesh()->GetSocketLocation(Target->HeadSocketName);
+
+		
 
 		FVector LastSeenLocation =BBComp->GetValue<UBlackboardKeyType_Vector>(BlackboardKey.GetSelectedKeyID());
 		FRotator LastSeenRotation = BBComp->GetValue<UBlackboardKeyType_Rotator>(LastRotatorKey.GetSelectedKeyID());
@@ -44,9 +57,11 @@ EBTNodeResult::Type UBTTask_GetChaseLocation::ExecuteTask(UBehaviorTreeComponent
 
 		FVector FinalSearchLocation = FVector(0.f);
 
+
 		//마지막 Target의 높이와 현재Target의 높이가 다른경우 수색 위치를 마지막 식별위치로 대입하고 끝낸다.
-		if (CurrentTargetLocation.Z != LastSeenLocation.Z || LastSeenLocation.Z != OwnerComp.GetOwner()->GetActorLocation().Z)
+		if (!FMath::IsNearlyEqual(CurrentTargetLocation.Z, LastSeenLocation.Z, 1.f) || !FMath::IsNearlyEqual(OwnerComp.GetOwner()->GetActorLocation().Z, LastSeenLocation.Z, 1.f) )
 		{
+			UE_LOG(LogTemp,Warning,TEXT("UBTTask_GetChaseLocation::ExecuteTask // Last Seen Location's Z value is different. Set FinalSerachLocation To Last Seen Location"));
 			FinalSearchLocation = LastSeenLocation;
 		}
 		//높이가 같은경우 == Target이 코너나, 장애물 사이로 들어가 숨은 경우임.
@@ -68,6 +83,10 @@ EBTNodeResult::Type UBTTask_GetChaseLocation::ExecuteTask(UBehaviorTreeComponent
 			//현재 이 OwnerAI의 위치값에 위에서 구한 방향+거리벡터를 더해 최종 수색 위치를 결정한다.
 			FinalSearchLocation = OwnerComp.GetOwner()->GetActorLocation() + PreCalc;
 		}
+
+		DrawDebugSphere(GetWorld(), LastSeenLocation, 8.f, 6, FColor::Purple, false, 10.f, 0, 3.f);
+		DrawDebugSphere(GetWorld(), CurrentTargetLocation, 8.f, 6, FColor::Green, false, 10.f, 0, 3.f);
+		DrawDebugSphere(GetWorld(), FinalSearchLocation, 8.f, 6, FColor::Red, false, 10.f, 0, 3.f);
 
 		//구한 최종 SearchLocation을 BBKey에 Set한다.
 		BBComp->SetValueAsVector(AICon->EstimatedLocationOfTargetKey,FinalSearchLocation);
