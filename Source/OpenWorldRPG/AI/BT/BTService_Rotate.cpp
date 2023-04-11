4c8722737a7b4ddc45a340ac56372e24792176b8
BigMandu <2only1@naver.com>
2023-04-11 13:14:45 +0900

    Update BTService_Rotate.cpp

--- Source/OpenWorldRPG/AI/BT/BTService_Rotate.cpp
+++ Source/OpenWorldRPG/AI/BT/BTService_Rotate.cpp
@@ -6,6 +6,7 @@
 #include "OpenWorldRPG/AI/EnemyAIController.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "BTService_Rotate.h"
+#include <DrawDebugHelpers.h>
 
 UBTService_Rotate::UBTService_Rotate()
 {
@@ -26,8 +27,34 @@ void UBTService_Rotate::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeM
 	AEnemyCharacter* AIChar = Cast<AEnemyCharacter>(AICon->GetCharacter());
 
 	//코너 탐지후 회전
+	
 	UPathFollowingComponent* PathFollowComp = AICon->GetPathFollowingComponent();
-	if(PathFollowComp == nullptr ) return;
-	TArray<FNavPathPoint> PathPoint = PathFollowComp->Getpath
+	if ( PathFollowComp == nullptr ) return;
+	const FNavPathSharedPtr Path = PathFollowComp->GetPath();
+	if(Path.IsValid() == false ) return ;
+	
+	
+	TArray<FNavPathPoint> PathPoint = Path->GetPathPoints();
+
+	int32 PathPointNum = PathPoint.Num();
+	if ( PathPointNum >= 3 )
+	{
+		for ( auto CornerPoint : PathPoint )
+		{
+			DrawDebugSphere(GetWorld(), CornerPoint.Location, 8.f, 6, FColor::Purple, false, 10.f, 0, 3.f);
+		}
+		
+	}
+
+
+
+	/*PathFollowComp->GetNextPathIndex();
+
+	const FNavPathPoint& NextPathPt = Path->GetPathPoints()[ PathFollowComp->GetNextPathIndex()];
+	if ( NextPathPt.HasNodeRef() )
+	{
+
+	}*/
+	
 
 }
